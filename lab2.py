a92a8a527e1b3cbbfb63f4cf8bed0d8cfc49653a
bramjot14 <bramjot14@users.noreply.github.com>
2025-01-14 10:48:08 -0500

    Added lab2.py file

---
+++ lab2.py
@@ -0,0 +1,27 @@
+elements = ["Hydrgen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon"]
+#print()"Elements: ", elements)# git add . && git commit -m "add elements array" && git push
+
+#def func_name();
+# return True
+#def say_greeting(name, message="h1");
+# print(f" {message}, {name}")
+# say_greeting("Bramjot")
+# say_greeting("Bramjot", "Hello")
+
+# def get_valid_int_input(prompt):
+#     while True:
+#         try:
+#             return int(input(print))
+#         except ValueError:
+#             print("Error: Please enter a valid integer!")
+#             continue
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/bramjotsingh/Lab2.git
+git push -u origin main
+
+git add COMP2152/lab2.py
+
